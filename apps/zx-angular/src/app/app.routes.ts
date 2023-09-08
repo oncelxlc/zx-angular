be7e1d630f096a30b963edb3cf08cd68949942c3
ZheXian <oncelxlc@hotmail.com>
2023-09-08 17:40:53 +0800

    Implement HomeComponent and modular file structure

--- apps/zx-angular/src/app/app.routes.ts
+++ apps/zx-angular/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('./common/home/home.component').then(c => c.HomeComponent),
+  },
+  {
+    path: 'docs',
+    loadChildren: () => import('./docs/docs.routes').then(m => m.docsRoutes),
+  },
+  {
+    path: 'experimental',
+    loadChildren: () => import('./experimental/experimental.routes').then(m => m.experimentalRoutes),
+  },
+  {
+    path: 'knowledge',
+    loadChildren: () => import('./knowledge/knowledge.routes').then(m => m.knowledgeRoutes),
+  },
+];
--- apps/zx-angular/src/app/common/header/header.component.ts
+++ apps/zx-angular/src/app/common/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'zxa-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
