1a2c957043bf25653ebc805488678e34907f93a7
ZheXian <oncelxlc@hotmail.com>
2023-10-25 18:07:58 +0800

    Refactor nav component to a module in zx-ui library

--- apps/zx-angular/src/app/common/header/header.component.ts
+++ apps/zx-angular/src/app/common/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NzGridModule } from 'ng-zorro-antd/grid';
-import { Nav_Components } from 'ng-zx'
+import { ZxNavModule } from 'ng-zx'
 
 @Component({
   selector: 'zxa-header',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, NzGridModule, Nav_Components],
+  imports: [CommonModule, NgOptimizedImage, NzGridModule, ZxNavModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
--- libs/zx-ui/src/lib/nav/index.ts
+++ libs/zx-ui/src/lib/nav/index.ts
@@ -1,4 +1,13 @@
 import { ZxNavComponent } from './nav.component';
 import { ZxNavItemComponent } from './nav-item.component'
+import { NgModule } from '@angular/core'
 
-export const Nav_Components = [ZxNavComponent, ZxNavItemComponent] as const;
+/**
+ * ZxNavModule 是一个用于在 Angular 应用程序中提供导航功能的模块。
+ * 它导出 ZxNavComponent 和 ZxNavItemComponent，并导入它们以在模块中使用。
+ */
+@NgModule({
+  imports: [ZxNavComponent, ZxNavItemComponent],
+  exports: [ZxNavComponent, ZxNavItemComponent]
+})
+export class ZxNavModule {}
