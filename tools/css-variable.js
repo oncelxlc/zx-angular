9f69b8f545a50a72a45b4a73c569cc57d90b9f01
ZheXian <oncelxlc@hotmail.com>
2023-11-16 14:08:45 +0800

    Adjust base color generation method

--- tools/css-variable.js
+++ tools/css-variable.js
@@ -2,12 +2,20 @@ const fs = require('fs');
 const {presetPalettes} = require('@ant-design/colors');
 
 const keys = Object.keys(presetPalettes);
-let context = `:root {`;
+let context = `  // @common-color\n`;
 for (const key of keys){
   presetPalettes[key].forEach((item, index) => {
-    context += `--base-${key}-${index}: ${item};`;
+    context += `  --base-${key}-${index}: ${item.toUpperCase()};\n`;
   });
 }
-context += '}';
 
-fs.writeFileSync('../components/styles/variables.scss', context);
+context += `  // @common-color-end\n`;
+
+const scssFilePath = '../components/styles/core/_variable-css.scss';
+const scssContent = fs.readFileSync(scssFilePath, 'utf8');
+const rootContent = scssContent.match(/:root\s*{[\S\s]*(\S{0}})/)[0]
+const scssContents = rootContent.split('}');
+scssContents[scssContents.length - 2] = scssContents[scssContents.length - 2].replace(/\n\s*\/\/ @common-color([\s\S]*?)\/\/ @common-color-end/g, '') + '\n' + context;
+const newScssContent = scssContents.join('}');
+const newContent = scssContent.replace(rootContent, newScssContent);
+fs.writeFileSync(scssFilePath, newContent, 'utf8');
