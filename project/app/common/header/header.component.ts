2a42790d4ddb73a83798d7d508ea096db05f7ddc
ZheXian <oncelxlc@hotmail.com>
2023-11-27 18:05:41 +0800

    feat(lib): 修改文件的导出方式

--- project/app/common/header/header.component.ts
+++ project/app/common/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { ZxNavComponent } from 'zx-ui/nav'
 
 @Component({
   selector: 'zxa-header',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, NzGridModule],
+  imports: [CommonModule, NgOptimizedImage, NzGridModule, ZxNavComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
