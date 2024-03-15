03854c7ea09afa933a333a89ea9791871c06b6fc
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-03-15 17:45:44 +0800

    feat(): 重新确定项目内容

--- project/app/app.component.ts
+++ project/app/app.component.ts
@@ -1,14 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from "@angular/router";
-import {MultiLevelDirectoryComponent, ZxaFooterComponent} from './common';
-import {ZxaHeaderComponent} from './common';
-import {ZxaFirstNavComponent} from './common';
 
 @Component({
   selector: 'zxa-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ZxaFooterComponent, ZxaHeaderComponent, ZxaFirstNavComponent, MultiLevelDirectoryComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- project/app/common/public-api.ts
+++ project/app/common/public-api.ts
@@ -1,4 +1,1 @@
-export * from './header/header.component';
 export * from './footer/footer.component';
-export * from './first-nav/first-nav.component';
-export * from './multi-level-directory/multi-level-directory.component';
