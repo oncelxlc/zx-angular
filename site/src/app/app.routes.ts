a9e449c74a2cc218b17305e47f32ccd835b0af72
ZheXian <oncelxlc@hotmail.com>
2024-03-31 22:49:54 +0800

    feat(): 新增首页及不确定页面

--- site/src/app/app.routes.ts
+++ site/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../pages/index/index.component').then(m => m.IndexComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('../pages/blog/blog.component').then(m => m.BlogComponent),
+  },
+  {
+    path: 'experiment',
+    loadChildren: () => import('../pages/experiment/experiment.component').then(m => m.ExperimentComponent),
+  },
+  {
+    path: 'notes',
+    loadChildren: () => import('../pages/notes/notes.component').then(m => m.NotesComponent),
+  },
+  {
+    path: 'not-found',
+    loadChildren: () => import('../pages/not-found/not-found.component').then(m => m.NotFoundComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
---
+++ site/src/pages/index/index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zxa-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss'
+})
+export class IndexComponent {
+
+}
