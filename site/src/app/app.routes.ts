15602a30c6d3c4e901924f1ee9561f56308612ee
ZheXian <oncelxlc@hotmail.com>
2024-04-02 23:55:09 +0800

    feat(): 界面想法

--- site/src/app/app.routes.ts
+++ site/src/app/app.routes.ts
@@ -3,24 +3,24 @@ import {Routes} from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('../pages/index/index.component').then(m => m.IndexComponent),
+    loadComponent: () => import('../pages/index/index.component').then(m => m.IndexComponent),
     pathMatch: 'full'
   },
   {
     path: 'blog',
-    loadChildren: () => import('../pages/blog/blog.component').then(m => m.BlogComponent),
+    loadComponent: () => import('../pages/blog/blog.component').then(m => m.BlogComponent),
   },
   {
     path: 'experiment',
-    loadChildren: () => import('../pages/experiment/experiment.component').then(m => m.ExperimentComponent),
+    loadComponent: () => import('../pages/experiment/experiment.component').then(m => m.ExperimentComponent),
   },
   {
     path: 'notes',
-    loadChildren: () => import('../pages/notes/notes.component').then(m => m.NotesComponent),
+    loadComponent: () => import('../pages/notes/notes.component').then(m => m.NotesComponent),
   },
   {
     path: 'not-found',
-    loadChildren: () => import('../pages/not-found/not-found.component').then(m => m.NotFoundComponent),
+    loadComponent: () => import('../pages/not-found/not-found.component').then(m => m.NotFoundComponent),
   },
   {
     path: '**',
