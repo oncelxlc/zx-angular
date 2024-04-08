2031f2e3b99b1831f3d982b144d6ac32bedd71de
ZheXian <oncelxlc@hotmail.com>
2024-04-08 20:24:04 +0800

    feat(): 主界面修改

--- site/src/app/app.component.spec.ts
+++ site/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zx-ng' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('zx-ng');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import { MainComponent } from "@zx-ng/app/components/main/main.component";
 
 @Component({
   selector: 'zxa-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'zx-ng';
 }
