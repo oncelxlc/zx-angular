ea7f9b53df02cf2e194c43a06b85348cb3dfceca
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-04-09 15:34:22 +0800

    feat(build): 修改自定义打包结构

--- builders/dist/src/md-builder.d.ts
+++
@@ -1,7 +0,0 @@
-import { JsonObject } from '@angular-devkit/core';
-interface Options extends JsonObject {
-    source: string;
-    destination: string;
-}
-declare const _default: import("@angular-devkit/architect/src/internal").Builder<Options & JsonObject>;
-export default _default;
