1f1cc77041b2cb8b41b17541b12b7dd79b0d5f35
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-04-10 10:55:58 +0800

    feat(main): 新增主界面header和footer组件

--- site/src/app/components/main/main.component.ts
+++ site/src/app/components/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '@zx-ng/app/components/header/header.component';
+import {FooterComponent} from '@zx-ng/app/components/footer/footer.component';
 
 @Component({
   selector: 'zxa-main',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
