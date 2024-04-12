f3c9f8e73c29ab6f20758a51101e603de39356de
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-04-12 18:02:56 +0800

    feat(): 修改footer内容

--- site/src/app/components/footer/footer.component.ts
+++ site/src/app/components/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'zxa-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
