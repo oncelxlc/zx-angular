2147a26977b37a51f1fcbaaef4531292344b8d3f
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-04-15 17:46:15 +0800

    feat(): 引入ng-zorro-antd组件库依赖

--- site/src/pages/not-found/not-found.component.ts
+++ site/src/pages/not-found/not-found.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  NzResultComponent, NzResultContentDirective, NzResultExtraDirective,
+  NzResultIconDirective,
+  NzResultSubtitleDirective, NzResultTitleDirective,
+} from 'ng-zorro-antd/result';
 
 @Component({
   selector: 'zxa-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    NzResultComponent,
+    NzResultIconDirective,
+    NzResultSubtitleDirective,
+    NzResultContentDirective,
+    NzResultExtraDirective,
+    NzResultTitleDirective,
+  ],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
 
