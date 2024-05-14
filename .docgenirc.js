7e5a05d84ecdc8342efe563f0347d7a3e8524043
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-05-14 13:01:48 +0800

    feat(): Update docgeni

---
+++ .docgenirc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('@docgeni/core').DocgeniConfig}
+ */
+module.exports = {
+    mode: 'full',
+    title: 'zx-angular',
+    description: 'Personal website developed based on Angular',
+    docsDir: 'docs',
+    navs: [
+        null
+    ],
+    libs: []
+};
---
+++ docs/getting-started.md
@@ -0,0 +1,14 @@
+---
+title: Getting Started
+order: 10
+---
+
+Hello Docgeni!
+
+## Installation
+
+```
+ng add @docgeni/cli
+```
+...
+
