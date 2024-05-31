847a43d625d1c578bbad9b52508e868ff3f9eeff
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-05-31 18:10:52 +0800

    fix: 修改基础顶部路由指示代码位置为app

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,13 +1,66 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {MainComponent} from '@zx-ng/app/components';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd, NavigationError, NavigationSkipped,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { MainComponent, PROGRESS_BAR_DELAY } from '@zx-ng/app/components';
+import { NgProgressComponent } from 'ngx-progressbar';
+import { filter, map, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'zxa-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, MainComponent, NgProgressComponent],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly router = inject(Router);
+
+  @ViewChild(NgProgressComponent, {static: true}) progressBar!: NgProgressComponent;
+
+  isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
+  ngOnInit(): void {
+    this.setupPageNavigationDimming();
+  }
+
+  private setupPageNavigationDimming() {
+    if (!this.isBrowser) {
+      return;
+    }
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationStart),
+        map(() => {
+          // Only apply set the property if the navigation is not "immediate"
+          return setTimeout(() => {
+            this.progressBar.start();
+          }, PROGRESS_BAR_DELAY);
+        }),
+        switchMap((timeoutId) => {
+          return this.router.events.pipe(
+            filter((e) => {
+              return (
+                e instanceof NavigationEnd ||
+                e instanceof NavigationCancel ||
+                e instanceof NavigationSkipped ||
+                e instanceof NavigationError
+              );
+            }),
+            take(1),
+            map(() => timeoutId),
+          );
+        }),
+      )
+      .subscribe((timeoutId) => {
+        // When the navigation finishes, prevent the navigating class from being applied in the timeout.
+        clearTimeout(timeoutId);
+        this.progressBar.complete();
+      });
+  }
 }
--- site/src/app/components/main/main.component.ts
+++ site/src/app/components/main/main.component.ts
@@ -1,16 +1,7 @@
-import {isPlatformBrowser} from '@angular/common';
-import {Component, inject, OnInit, PLATFORM_ID, ViewChild} from '@angular/core';
-import {
-  NavigationCancel,
-  NavigationEnd, NavigationError,
-  NavigationSkipped,
-  NavigationStart,
-  Router,
-} from '@angular/router';
-import {HeaderComponent} from '@zx-ng/app/components/header/header.component';
-import {FooterComponent} from '@zx-ng/app/components/footer/footer.component';
-import {NgProgressComponent} from 'ngx-progressbar';
-import {filter, map, switchMap, take} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '@zx-ng/app/components/header/header.component';
+import { FooterComponent } from '@zx-ng/app/components/footer/footer.component';
+import { NgProgressComponent } from 'ngx-progressbar';
 
 export const PROGRESS_BAR_DELAY = 30;
 
@@ -26,51 +17,9 @@ export const PROGRESS_BAR_DELAY = 30;
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  private readonly router = inject(Router);
-
-  @ViewChild(NgProgressComponent, {static: true}) progressBar!: NgProgressComponent;
-
-  isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
-
   constructor() {
   }
 
   ngOnInit(): void {
-    this.setupPageNavigationDimming();
-  }
-
-  private setupPageNavigationDimming() {
-    if (!this.isBrowser) {
-      return;
-    }
-    this.router.events
-      .pipe(
-        filter((e) => e instanceof NavigationStart),
-        map(() => {
-          // Only apply set the property if the navigation is not "immediate"
-          return setTimeout(() => {
-            this.progressBar.start();
-          }, PROGRESS_BAR_DELAY);
-        }),
-        switchMap((timeoutId) => {
-          return this.router.events.pipe(
-            filter((e) => {
-              return (
-                e instanceof NavigationEnd ||
-                e instanceof NavigationCancel ||
-                e instanceof NavigationSkipped ||
-                e instanceof NavigationError
-              );
-            }),
-            take(1),
-            map(() => timeoutId),
-          );
-        }),
-      )
-      .subscribe((timeoutId) => {
-        // When the navigation finishes, prevent the navigating class from being applied in the timeout.
-        clearTimeout(timeoutId);
-        this.progressBar.complete();
-      });
   }
 }
