296545113c70cf5afae53a3593f84d612018c673
ZheXian <oncelxlc@hotmail.com>
2024-06-23 16:26:10 +0800

    fix: 修改全局基础样式,修改主界面最低高度设定方式

--- site/src/app/components/main/main.component.ts
+++ site/src/app/components/main/main.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { HeaderComponent } from '@zx-ng/app/components/header/header.component';
-import { FooterComponent } from '@zx-ng/app/components/footer/footer.component';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FooterComponent } from "@zx-ng/app/components/footer/footer.component";
+import { HeaderComponent } from "@zx-ng/app/components/header/header.component";
 
 export const PROGRESS_BAR_DELAY = 30;
 
 @Component({
-  selector: 'zxa-main',
+  selector: "zxa-main",
   standalone: true,
   imports: [
     HeaderComponent,
     FooterComponent,
   ],
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss'],
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
+  footerHeight: number = 0;
+  @ViewChild("footer") private footer!: ElementRef<any>;
+
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.footerHeight = this.footer && this.footer.nativeElement.offsetHeight || 100;
+    });
   }
 }
