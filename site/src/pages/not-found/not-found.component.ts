4b28b10826624bebd6be0599bbd978de3e18023f
ZheXian <oncelxlc@hotmail.com>
2024-08-03 00:02:08 +0800

    fix: 移除 ng-zorro-antd 相关内容

--- site/src/pages/not-found/not-found.component.ts
+++ site/src/pages/not-found/not-found.component.ts
@@ -1,25 +1,12 @@
 import {NgOptimizedImage} from '@angular/common';
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {NzButtonComponent} from 'ng-zorro-antd/button';
-import {
-  NzResultComponent, NzResultContentDirective, NzResultExtraDirective,
-  NzResultIconDirective,
-  NzResultSubtitleDirective, NzResultTitleDirective,
-} from 'ng-zorro-antd/result';
 
 @Component({
   selector: 'zxa-not-found',
   standalone: true,
   imports: [
-    NzResultComponent,
-    NzResultIconDirective,
-    NzResultSubtitleDirective,
-    NzResultContentDirective,
-    NzResultExtraDirective,
-    NzResultTitleDirective,
     NgOptimizedImage,
-    NzButtonComponent,
     RouterLink,
   ],
   templateUrl: './not-found.component.html',
