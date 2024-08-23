df4103d1c6979c5356afd3e120a248c24306f392
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-08-23 17:58:19 +0800

    fix: 修改主界面使用 HeightObserverDirective

--- site/src/app/components/main/main.component.ts
+++ site/src/app/components/main/main.component.ts
@@ -1,17 +1,18 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
-import { FooterComponent } from "../footer/footer.component";
-import { HeaderComponent } from "../header/header.component";
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { HeightObserverDirective } from '@ngx-zx/directive';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
-  selector: "zxa-main",
+  selector: 'zxa-main',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
-  templateUrl: "./main.component.html",
-  styleUrls: ["./main.component.scss"],
+  imports: [HeaderComponent, FooterComponent, HeightObserverDirective],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements AfterViewInit {
   footerHeight = 0;
-  @ViewChild("footer")
+  @ViewChild('footer')
   private footer!: ElementRef<HTMLDivElement>;
 
   ngAfterViewInit(): void {
