70dace822e44143779ba2bcb9038f5e10c362897
zhexian <84336243+oncelxlc@users.noreply.github.com>
2024-09-12 18:07:15 +0800

    fix: 在 table 组件中配置 encapsulation: ViewEncapsulation.None 不进行视图封装、样式隔离

--- components/table/table.component.ts
+++ components/table/table.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'zx-table',
   standalone: true,
   imports: [],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class TableComponent {
 
