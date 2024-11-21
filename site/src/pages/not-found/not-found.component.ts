9c8eddaeaadef4725dc7e7360e5f1297b2886e3d
ZheXian <oncelxlc@hotmail.com>
2024-11-21 20:13:25 +0800

    fix: 修复 scss 不支持 @import 的的报错

--- site/src/pages/not-found/not-found.component.ts
+++ site/src/pages/not-found/not-found.component.ts
@@ -1,14 +1,9 @@
-import {NgOptimizedImage} from '@angular/common';
 import {Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'zxa-not-found',
   standalone: true,
-  imports: [
-    NgOptimizedImage,
-    RouterLink,
-  ],
+  imports: [],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
