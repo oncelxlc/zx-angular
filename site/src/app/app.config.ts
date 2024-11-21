83df2d0116ff358511d34ddb286ab71890baf3be
ZheXian <oncelxlc@hotmail.com>
2024-11-22 00:48:27 +0800

    feat(site): 添加 Material 主题并更新动画配置
    - 在 styles 中添加 Azure Blue 主题样式
    - 在 app.config.ts 中引入 BrowserAnimationsModule

--- site/src/app/app.config.ts
+++ site/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()],
+  providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync(), BrowserAnimationsModule],
 };
