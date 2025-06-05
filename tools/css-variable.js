58a2e9f43fd28869bd7a17d1dec2fdcc55b3a41f
zhexian <84336243+oncelxlc@users.noreply.github.com>
2025-06-05 17:48:40 +0800

    refactor(styles): 合并颜色和尺寸变量，更新布局结构

--- tools/css-variable.js
+++ tools/css-variable.js
@@ -3,15 +3,15 @@ const {presetPalettes} = require('@ant-design/colors');
 
 const keys = Object.keys(presetPalettes);
 let context = `  // @common-color\n`;
-for (const key of keys){
+for (const key of keys) {
   presetPalettes[key].forEach((item, index) => {
-    context += `  --base-${key}-${index}: ${item.toUpperCase()};\n`;
+    context += `  --base-${key}-${index + 1}: ${item.toUpperCase()};\n`;
   });
 }
 
 context += `  // @common-color-end\n`;
 
-const scssFilePath = '../components/styles/core/_base-color.scss';
+const scssFilePath = 'site/src/styles/basic/_base-color.scss';
 const scssContent = fs.readFileSync(scssFilePath, 'utf8');
 const rootContent = scssContent.match(/:root\s*{[\S\s]*(\S{0}})/)[0]
 const scssContents = rootContent.split('}');
