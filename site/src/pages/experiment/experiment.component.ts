c14dbcbb3a112564c1f55d572bf6c23c2ff1ad0e
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2025-06-13 00:10:16 +0800

    refactor(tsconfig): 重构 TypeScript 和 Angular 编译配置，优化构建设置

--- site/src/pages/experiment/experiment.component.ts
+++ site/src/pages/experiment/experiment.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { TableComponent } from '@ngx-zx/table';
 
 @Component({
   selector: 'zxa-experiment',
   imports: [
-    TableComponent,
   ],
   templateUrl: './experiment.component.html',
   styleUrl: './experiment.component.scss'
