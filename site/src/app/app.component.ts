a05dcc1328ef2b8f09a4d1fe8818f346d33d29b6
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2025-06-20 22:29:44 +0800

    chore(tsconfig): 添加 tsconfig.json 并调整 tsconfig.app.json 的继承路径以优化配置结构

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgProgressbar } from 'ngx-progressbar';
-import { NgProgressRouter } from 'ngx-progressbar/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgProgressbar } from "ngx-progressbar";
+import { NgProgressRouter } from "ngx-progressbar/router";
 
 @Component({
-  selector: 'zxa-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  selector: "zxa-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
   imports: [RouterOutlet, NgProgressbar, NgProgressRouter],
 })
 export class AppComponent {
