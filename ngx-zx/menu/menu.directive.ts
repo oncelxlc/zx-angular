77aff603ee6b8212da882c36e8c88d2e59cc43f9
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2025-07-21 00:23:33 +0800

    feat(menu): enhance MenuDirective with host bindings and input property

--- ngx-zx/menu/menu.directive.ts
+++ ngx-zx/menu/menu.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
 
 @Directive({
   selector: "[zxMenu]",
+  host: {
+    class: "zx-menu",
+    "[class.zx-menu-vertical]": "zxMode() === 'vertical'",
+    "[class.zx-menu-inline]": "zxMode() === 'inline'",
+  }
 })
 export class MenuDirective {
-
+  zxMode = input<"inline" | "vertical">("vertical");
 }
