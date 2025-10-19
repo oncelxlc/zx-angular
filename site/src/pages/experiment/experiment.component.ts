d3bbda32fbe4ec8a5972f901008940cab88119ec
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2025-10-19 11:50:57 +0800

    chore(前端组件): 移除 Monaco 组件及相关配置

--- site/src/pages/experiment/experiment.component.ts
+++ site/src/pages/experiment/experiment.component.ts
@@ -1,12 +1,10 @@
 import { Component } from "@angular/core";
 import { ResizeObserverDirective, SizeData } from "@ngx-zx/directive";
-import { MonacoComponent } from "@ngx-zx/editor";
 
 @Component({
   selector: "zxa-experiment",
   imports: [
     ResizeObserverDirective,
-    MonacoComponent,
   ],
   templateUrl: "./experiment.component.html",
   styleUrl: "./experiment.component.scss",
